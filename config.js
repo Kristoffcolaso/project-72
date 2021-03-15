eecd07cb4d36c96506d71266558581b224eccf93
Kristoffcolaso <73115005+Kristoffcolaso@users.noreply.github.com>
2021-03-15 19:49:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+required('@firebase/firestore');
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiJPsKunWy84QXg_rLrmtogQcr8UtDRYk",
+  authDomain: "wireleibrary-e2eb6.firebaseapp.com",
+  projectId: "wireleibrary-e2eb6",
+  storageBucket: "wireleibrary-e2eb6.appspot.com",
+  messagingSenderId: "135324235239",
+  appId: "1:135324235239:web:168288c48d6986504bf942",
+  measurementId: "G-W3SX2GPVZC"
+};
+firebase.innitializeApp(firebaseConfig);
+
+export default firebase.firestore();
